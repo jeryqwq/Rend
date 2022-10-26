042f2bd314090aacebcc73ea648878b2f817fe5c
chenjie <jery1997@foxmail.com>
2022-10-26 14:09:05 +0800

    update

---
+++ src/pages/back/order/index.tsx
@@ -0,0 +1,318 @@
+import React, { Ref, useEffect, useRef, useState } from 'react';
+import styles from './index.module.less';
+import { Button, message, Modal } from 'antd';
+import {
+  ActionType,
+  ProForm,
+  ProFormInstance,
+  ProFormText,
+  ProTable,
+} from '@ant-design/pro-components';
+import { commonRequest } from '@/server/common';
+import { useHistory } from 'umi';
+import { orderStatus } from '@/constants/var';
+import dayjs from 'dayjs';
+function Repair() {
+  const history = useHistory();
+  const tableRef = useRef<ActionType>();
+  const [typeEnum, setEnum] = useState({});
+  useEffect(() => {
+    (async () => {
+      const res = await commonRequest('/mallBrandInfo/all', {
+        method: 'post',
+        data: {},
+      });
+      if (res.code === '0') {
+        let _data: any = {};
+        res.data.forEach((i: any) => {
+          _data[i.brandName] = i.brandName;
+        });
+        setEnum(_data);
+      }
+    })();
+  }, []);
+  return (
+    <div className="content">
+      <div className={styles['repair-wrap']}>
+        <div className="tit">
+          <span style={{ background: 'white', paddingRight: 10 }}>
+            订单管理
+          </span>
+        </div>
+        <Button
+          type="primary"
+          style={{ float: 'right' }}
+          onClick={() => {
+            history.push('/repair');
+          }}
+        >
+          新增
+        </Button>
+        <ProTable
+          rowKey={'id'}
+          columns={[
+            {
+              dataIndex: 'id',
+              title: '订单ID',
+              ellipsis: true,
+              copyable: true,
+              hideInSearch: true,
+            },
+            // {
+            //   dataIndex: 'cityName',
+            //   title: '省',
+            // },{
+            //   dataIndex: 'countyName',
+            //   title: '市',
+            // },
+            {
+              dataIndex: 'createTime',
+              hideInSearch: true,
+              ellipsis: true,
+              title: '下单时间',
+            },
+            {
+              dataIndex: 'orderSn',
+              ellipsis: true,
+              title: '订单号',
+            },
+            {
+              dataIndex: 'orderStatus',
+              title: '订单状态',
+              valueEnum: {
+                '-1': '取消支付',
+                1: '下单待支付',
+                2: '已经支付',
+                3: '已经发货',
+                10: '结束',
+              },
+            },
+            {
+              dataIndex: 'orderType',
+              title: '订单类型',
+              valueEnum: {
+                1: '租赁',
+                2: '购买',
+              },
+            },
+            {
+              dataIndex: 'orderMoney',
+              hideInSearch: true,
+              ellipsis: true,
+              title: '订单金额',
+            },
+
+            {
+              dataIndex: 'address',
+              hideInSearch: true,
+              ellipsis: true,
+              title: '地址',
+            },
+            {
+              dataIndex: 'contactNumber',
+              title: '手机号',
+              hideInSearch: true,
+            },
+            {
+              dataIndex: 'id',
+              title: '操作',
+              hideInSearch: true,
+              render(text, item) {
+                console.log(item, '--');
+                return (
+                  <>
+                    {item.orderPayStatus === 10 && (
+                      <Button
+                        type="link"
+                        onClick={async () => {
+                          await commonRequest(
+                            '/mallOrderMaster/paid/' + item.id,
+                            {
+                              method: 'post',
+                              data: {},
+                            },
+                          );
+                          message.success('审核成功！');
+                        }}
+                      >
+                        审核
+                      </Button>
+                    )}
+                    {
+                      // orderStatus === 2
+                      item.orderStatus === 2 && (
+                        <Button
+                          type="link"
+                          onClick={async () => {
+                            let _temp = {},
+                              _ref: React.MutableRefObject<
+                                ProFormInstance<any>
+                              >;
+                            function TempComp() {
+                              _ref = useRef<ProFormInstance>();
+                              return (
+                                <ProForm
+                                  formRef={_ref}
+                                  submitter={false}
+                                  onValuesChange={(val, values) => {
+                                    console.log(values);
+                                    _temp = values;
+                                  }}
+                                >
+                                  <ProFormText
+                                    name="shippingCompName"
+                                    label="快递公司"
+                                    rules={[{ required: true }]}
+                                  />
+                                  <ProFormText
+                                    label="快递单号	"
+                                    required
+                                    rules={[{ required: true }]}
+                                    name="shippingSn"
+                                  />
+                                </ProForm>
+                              );
+                            }
+                            Modal.confirm({
+                              icon: null,
+                              content: <TempComp />,
+                              async onOk() {
+                                const res =
+                                  await _ref?.current?.getFieldsValue();
+                                await commonRequest(
+                                  '/mallOrderMaster/translate',
+                                  {
+                                    method: 'post',
+                                    data: {
+                                      ...res,
+                                      shippingTime: dayjs(new Date()),
+                                    },
+                                  },
+                                );
+                                message.success('发货成功！');
+                              },
+                            });
+                          }}
+                        >
+                          发货
+                        </Button>
+                      )
+                    }
+                    {item.orderStatus === 3 && (
+                      <Button
+                        type="link"
+                        onClick={async () => {
+                          await commonRequest(
+                            '/mallOrderMaster/complete/' + item.id,
+                            {
+                              method: 'post',
+                              data: {},
+                            },
+                          );
+                          message.success('订单已结束!');
+                        }}
+                      >
+                        结束
+                      </Button>
+                    )}
+                  </>
+                );
+              },
+            },
+          ]}
+          pagination={{
+            pageSize: 10,
+          }}
+          expandable={{
+            expandedRowRender(item) {
+              return (
+                <ProTable
+                  search={false}
+                  dataSource={item.details || []}
+                  columns={[
+                    {
+                      dataIndex: 'equipAmount',
+                      title: '数量',
+                    },
+                    {
+                      dataIndex: 'equipBrand',
+                      title: '品牌',
+                    },
+                    {
+                      dataIndex: 'equipId',
+                      title: 'id',
+                    },
+                    {
+                      dataIndex: 'equipName',
+                      title: '名称',
+                    },
+                    {
+                      dataIndex: 'equipPrice',
+                      title: '单价',
+                    },
+                    {
+                      dataIndex: 'mainImgPath',
+                      title: '图片',
+                      render(text) {
+                        return (
+                          <img
+                            src={'/lease-center/' + text}
+                            style={{ width: 80, height: 80 }}
+                          />
+                        );
+                      },
+                    },
+                    {
+                      dataIndex: 'equipAmount',
+                      title: '数量',
+                    },
+                  ]}
+                />
+              );
+            },
+          }}
+          actionRef={tableRef}
+          request={async ({ pageSize, current, ...others }) => {
+            let conditions = [];
+            others?.orderStatus !== undefined &&
+              conditions.push({
+                column: 'order_status',
+                operator: 'eq',
+                value: others?.orderStatus,
+              });
+            others?.orderSn !== undefined &&
+              conditions.push({
+                column: 'order_sn',
+                operator: 'like',
+                value: others?.orderSn,
+              });
+            others?.orderType !== undefined &&
+              conditions.push({
+                column: 'order_type',
+                operator: 'eq',
+                value: others?.orderType,
+              });
+            const res = await commonRequest('/mallOrderMaster/pageByBrand', {
+              data: {
+                size: pageSize,
+                current,
+                conditions,
+              },
+              method: 'post',
+            });
+            if (res.code === '0') {
+              return {
+                data: res.data.records,
+                total: res.data.total,
+              };
+            }
+            return {};
+          }}
+        />
+        <div className="page-wrap"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Repair;
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Button  } from 'antd'
+import { Button } from 'antd';
 const saleRouter = {
   path: '/saler',
   component: '@/layouts/back',
   routes: [
     {
-        path: '/saler',
-        label: '商家中心',
-        key:'/saler',
-        exact: true,
-        component: '@/pages/back/index',
+      path: '/saler',
+      label: '商家中心',
+      key: '/saler',
+      exact: true,
+      component: '@/pages/back/index',
     },
     {
       path: '/saler/product',
@@ -17,302 +17,322 @@ const saleRouter = {
       key: '/saler/product',
       exact: true,
       component: '@/pages/back/product/index',
-    },{
+    },
+    {
       path: '/saler/course',
       label: '培训管理',
       key: '/saler/course',
       exact: true,
       component: '@/pages/back/course/index',
-    }
-    ,{
+    },
+    {
       path: '/saler/repair',
       label: '维修管理',
       key: '/saler/repair',
       exact: true,
       component: '@/pages/back/repair/index',
     },
-  ]
-}
-  const routes  = { path: '/', component: '@/layouts/index', routes:[
-  {
-    path: '/',
-    label: '首页',
-    key: '/',
-    component: '@/pages/froent/index/index',
-    showmenu: 0
-  },
-  {
-    label: '全部设备',
-    key: '/allDevice',
-    path:'/allDevice',
-    component: '@/pages/froent/allDevice/index',
-  },
-  {
-    label: '设备租赁',
-    key: '/rentList',
-    path:'/rentList',
-    component: '@/pages/froent/rentList/index',
-  },
-  {
-    label: '新机出售',
-    key: '/newDevice',
-    path:'/newDevice',
-    component: '@/pages/froent/newDevice/index',
-  },
-  {
-    label: '二手设备',
-    key: '/sallList',
-    path:'/sallList',
-    component: '@/pages/froent/sallList/index',
-  },
- 
-  {
-    label: '设备维修',
-    key: '/repair',
-    path:'/repair',
-    component: '@/pages/froent/repair/index',
-  },
-  {
-    label: '技能培训',
-    key: '/course',
-    path:'/course',
-    component: '@/pages/froent/course/index',
-  },
-  {
-    label: '配件商城',
-    key: '/part/',
-    path:'/part',
-    component: '@/pages/froent/part/index',
-    other:  <Button type={'text'} style={{margin: '0 10px', color: '#666'}}
-    onClick={() => {
-      window.open('http://psi.fjrongshengda.com/')
-    }}
-  >进销存管理平台</Button>
-  },
-  {
-    label: '物流运输',
-    key: '/transport',
-    path:'/transport',
-    component: '@/pages/froent/transport/index',
-  },
-  {
-    label: '机手求职',
-    key: '/findJob',
-    path:'/findJob',
-    component: '@/pages/froent/findJob/index',
-  },
-  {
-    label: '行业动态',
-    key: '/news',
-    path:'/news',
-    component: '@/pages/froent/news/index',
-  },  {
-    label: '律师列表',
-    key: '/law',
-    path:'/law',
-    component: '@/pages/froent/news/law',
-    hide: true
-  },
-  {
-    label: '动态详情',
-    key: '/news/detail',
-    path:'/news/detail',
-    component: '@/pages/froent/news/detail',
-    hide: true
-  },
-  {
-    label: '新增零件',
-    key: '/addPart',
-    path:'/addPart',
-    component: '@/pages/froent/addPart',
-    hide: true
-  },
-  {
-    label: '新增课程',
-    key: '/addCourse',
-    path:'/addCourse',
-    component: '@/pages/froent/addCourse',
-    hide: true
-  },
-  {
-    label: '课程详情',
-    key: '/courseDetail',
-    path:'/courseDetail',
-    component: '@/pages/froent/courseDetail',
-    hide: true
-  },
-  {
-    label: '设备出租',
-    key: '/productRent',
-    path:'/productRent',
-    component: '@/pages/froent/productRent/index',
-    hide: true
-  },
-  {
-    label: '下单成功',
-    key: '/orderSuccess',
-    path:'/orderSuccess',
-    component: '@/pages/froent/orderSuccess/index',
-    hide: true
-  },
-  {
-    label: '地址选择',
-    key: '/orderAddress',
-    path:'/orderAddress',
-    component: '@/pages/froent/orderAddress/index',
-    hide: true
-  },
-  {
-    label: '求租设备',
-    key: '/forRent',
-    path:'/forRent',
-    component: '@/pages/froent/forRent/index',
-    hide: true
-  },
-  {
-    label: '配件详情',
-    key: '/partDetail',
-    path:'/partDetail',
-    component: '@/pages/froent/partDetail/index',
-    hide: true
-  },
-  {
-    label: '发布二手',
-    key: '/sallOld',
-    path:'/sallOld',
-    hide: true,
-    component: '@/pages/froent/sallOld/index',
-  },
-  {
-    label: '发布新机',
-    key: '/sallNew',
-    path:'/sallNew',
-    hide: true,
-    component: '@/pages/froent/sallNew/index',
-  },
-  {
-    label: '求购设备',
-    key: '/forBuy',
-    path:'/forBuy',
-    component: '@/pages/froent/forBuy/index',
-    hide: true
-  },
-  {
-    label: '求购新机',
-    key: '/forNew',
-    path:'/forNew',
-    component: '@/pages/froent/forNew/index',
-    hide: true
-  },
-  {
-    label: '设备详情',
-    key: '/productDetail',
-    path:'/productDetail',
-    component: '@/pages/froent/productDetail/index',
-    hide: true
-  },
-  {
-    label: '新机详情',
-    key: '/newDetail',
-    path:'/newDetail',
-    component: '@/pages/froent/newDetail/index',
-    hide: true
-  },
-  {
-    label: '出租详情',
-    key: '/rentDetail',
-    path:'/rentDetail',
-    component: '@/pages/froent/rentDetail/index',
-    hide: true
-  },
-  {
-    label: '购物车',
-    key: '/shoppingCart',
-    path:'/shoppingCart',
-    component: '@/pages/froent/shopCart/index',
-    hide: true
-  },
-  {
-    label: '品牌商认证',
-    key: '/salerAuth',
-    path:'/salerAuth',
-    component: '@/pages/froent/salerAuth/index',
-    hide: true
-  },
-  {
-    label: '施工单位认证',
-    key: '/buyAuth',
-    path:'/buyAuth',
-    component: '@/pages/froent/buyAuth/index',
-    hide: true
-  },
-  {
-    label: '品牌列表',
-    key: '/brandList',
-    path:'/brandList',
-    component: '@/pages/froent/brandList/index',
-    hide: true
-  },
-  {
-    label: '用户中心',
-    key: '/userCenter',
-    path:'/userCenter',
-    component: '@/layouts/user',
-    hide: true,
-    routes: [
-      {
-        label: '用户中心',
-        key: '/userCenter/',
-        path:'/userCenter/',
-        component: '@/pages/froent/user',
-      },
-      {
-        label: '我的订单',
-        key: '/userCenter/order',
-        path:'/userCenter/order',
-        component: '@/pages/froent/order',
-      },
-      {
-        label: '消息',
-        key: '/userCenter/message',
-        path:'/userCenter/message',
-        component: '@/pages/froent/message',
-      },
-      {
-        label: '个人信息',
-        key: '/userCenter/userInfo',
-        path:'/userCenter/userInfo',
-        component: '@/pages/froent/userInfo',
-      },
-      {
-        label: '安全管理',
-        key: '/userCenter/safeSetting',
-        path:'/userCenter/safeSetting',
-        component: '@/pages/froent/safeSetting',
-      },
-      {
-        label: '地址管理',
-        key: '/userCenter/address',
-        path:'/userCenter/address',
-        component: '@/pages/froent/address',
-      }
-      ,{
-        path: '/userCenter/myRepair',
-        label: '维修管理',
-        key: '/userCenter/myRepair',
-        exact: true,
-        component: '@/pages/back/myRepair/index',
-      }
-    ]
-  },
-] }
+    {
+      path: '/saler/order',
+      label: '订单管理',
+      key: '/saler/order',
+      exact: true,
+      component: '@/pages/back/order/index',
+    },
+  ],
+};
+const routes = {
+  path: '/',
+  component: '@/layouts/index',
+  routes: [
+    {
+      path: '/',
+      label: '首页',
+      key: '/',
+      component: '@/pages/froent/index/index',
+      showmenu: 0,
+    },
+    {
+      label: '全部设备',
+      key: '/allDevice',
+      path: '/allDevice',
+      component: '@/pages/froent/allDevice/index',
+    },
+    {
+      label: '设备租赁',
+      key: '/rentList',
+      path: '/rentList',
+      component: '@/pages/froent/rentList/index',
+    },
+    {
+      label: '新机出售',
+      key: '/newDevice',
+      path: '/newDevice',
+      component: '@/pages/froent/newDevice/index',
+    },
+    {
+      label: '二手设备',
+      key: '/sallList',
+      path: '/sallList',
+      component: '@/pages/froent/sallList/index',
+    },
 
-console.log('totalPage:', routes.routes.length + saleRouter.routes.length + 7)
-export const MenuRouter = routes.routes.filter(i => !i.hide)
-export const ShowSaleRouter = saleRouter.routes.filter(i => !i.hide)
+    {
+      label: '设备维修',
+      key: '/repair',
+      path: '/repair',
+      component: '@/pages/froent/repair/index',
+    },
+    {
+      label: '技能培训',
+      key: '/course',
+      path: '/course',
+      component: '@/pages/froent/course/index',
+    },
+    {
+      label: '配件商城',
+      key: '/part/',
+      path: '/part',
+      component: '@/pages/froent/part/index',
+      other: (
+        <Button
+          type={'text'}
+          style={{ margin: '0 10px', color: '#666' }}
+          onClick={() => {
+            window.open('http://psi.fjrongshengda.com/');
+          }}
+        >
+          进销存管理平台
+        </Button>
+      ),
+    },
+    {
+      label: '物流运输',
+      key: '/transport',
+      path: '/transport',
+      component: '@/pages/froent/transport/index',
+    },
+    {
+      label: '机手求职',
+      key: '/findJob',
+      path: '/findJob',
+      component: '@/pages/froent/findJob/index',
+    },
+    {
+      label: '行业动态',
+      key: '/news',
+      path: '/news',
+      component: '@/pages/froent/news/index',
+    },
+    {
+      label: '律师列表',
+      key: '/law',
+      path: '/law',
+      component: '@/pages/froent/news/law',
+      hide: true,
+    },
+    {
+      label: '动态详情',
+      key: '/news/detail',
+      path: '/news/detail',
+      component: '@/pages/froent/news/detail',
+      hide: true,
+    },
+    {
+      label: '新增零件',
+      key: '/addPart',
+      path: '/addPart',
+      component: '@/pages/froent/addPart',
+      hide: true,
+    },
+    {
+      label: '新增课程',
+      key: '/addCourse',
+      path: '/addCourse',
+      component: '@/pages/froent/addCourse',
+      hide: true,
+    },
+    {
+      label: '课程详情',
+      key: '/courseDetail',
+      path: '/courseDetail',
+      component: '@/pages/froent/courseDetail',
+      hide: true,
+    },
+    {
+      label: '设备出租',
+      key: '/productRent',
+      path: '/productRent',
+      component: '@/pages/froent/productRent/index',
+      hide: true,
+    },
+    {
+      label: '下单成功',
+      key: '/orderSuccess',
+      path: '/orderSuccess',
+      component: '@/pages/froent/orderSuccess/index',
+      hide: true,
+    },
+    {
+      label: '地址选择',
+      key: '/orderAddress',
+      path: '/orderAddress',
+      component: '@/pages/froent/orderAddress/index',
+      hide: true,
+    },
+    {
+      label: '求租设备',
+      key: '/forRent',
+      path: '/forRent',
+      component: '@/pages/froent/forRent/index',
+      hide: true,
+    },
+    {
+      label: '配件详情',
+      key: '/partDetail',
+      path: '/partDetail',
+      component: '@/pages/froent/partDetail/index',
+      hide: true,
+    },
+    {
+      label: '发布二手',
+      key: '/sallOld',
+      path: '/sallOld',
+      hide: true,
+      component: '@/pages/froent/sallOld/index',
+    },
+    {
+      label: '发布新机',
+      key: '/sallNew',
+      path: '/sallNew',
+      hide: true,
+      component: '@/pages/froent/sallNew/index',
+    },
+    {
+      label: '求购设备',
+      key: '/forBuy',
+      path: '/forBuy',
+      component: '@/pages/froent/forBuy/index',
+      hide: true,
+    },
+    {
+      label: '求购新机',
+      key: '/forNew',
+      path: '/forNew',
+      component: '@/pages/froent/forNew/index',
+      hide: true,
+    },
+    {
+      label: '设备详情',
+      key: '/productDetail',
+      path: '/productDetail',
+      component: '@/pages/froent/productDetail/index',
+      hide: true,
+    },
+    {
+      label: '新机详情',
+      key: '/newDetail',
+      path: '/newDetail',
+      component: '@/pages/froent/newDetail/index',
+      hide: true,
+    },
+    {
+      label: '出租详情',
+      key: '/rentDetail',
+      path: '/rentDetail',
+      component: '@/pages/froent/rentDetail/index',
+      hide: true,
+    },
+    {
+      label: '购物车',
+      key: '/shoppingCart',
+      path: '/shoppingCart',
+      component: '@/pages/froent/shopCart/index',
+      hide: true,
+    },
+    {
+      label: '品牌商认证',
+      key: '/salerAuth',
+      path: '/salerAuth',
+      component: '@/pages/froent/salerAuth/index',
+      hide: true,
+    },
+    {
+      label: '施工单位认证',
+      key: '/buyAuth',
+      path: '/buyAuth',
+      component: '@/pages/froent/buyAuth/index',
+      hide: true,
+    },
+    {
+      label: '品牌列表',
+      key: '/brandList',
+      path: '/brandList',
+      component: '@/pages/froent/brandList/index',
+      hide: true,
+    },
+    {
+      label: '用户中心',
+      key: '/userCenter',
+      path: '/userCenter',
+      component: '@/layouts/user',
+      hide: true,
+      routes: [
+        {
+          label: '用户中心',
+          key: '/userCenter/',
+          path: '/userCenter/',
+          component: '@/pages/froent/user',
+        },
+        {
+          label: '我的订单',
+          key: '/userCenter/order',
+          path: '/userCenter/order',
+          component: '@/pages/froent/order',
+        },
+        {
+          label: '消息',
+          key: '/userCenter/message',
+          path: '/userCenter/message',
+          component: '@/pages/froent/message',
+        },
+        {
+          label: '个人信息',
+          key: '/userCenter/userInfo',
+          path: '/userCenter/userInfo',
+          component: '@/pages/froent/userInfo',
+        },
+        {
+          label: '安全管理',
+          key: '/userCenter/safeSetting',
+          path: '/userCenter/safeSetting',
+          component: '@/pages/froent/safeSetting',
+        },
+        {
+          label: '地址管理',
+          key: '/userCenter/address',
+          path: '/userCenter/address',
+          component: '@/pages/froent/address',
+        },
+        {
+          path: '/userCenter/myRepair',
+          label: '维修管理',
+          key: '/userCenter/myRepair',
+          exact: true,
+          component: '@/pages/back/myRepair/index',
+        },
+      ],
+    },
+  ],
+};
+
+console.log('totalPage:', routes.routes.length + saleRouter.routes.length + 7);
+export const MenuRouter = routes.routes.filter((i) => !i.hide);
+export const ShowSaleRouter = saleRouter.routes.filter((i) => !i.hide);
 export default [
   {
-    path: '/login', component: '@/pages/login/index'
+    path: '/login',
+    component: '@/pages/login/index',
   },
   saleRouter,
   routes,
